bf2a6fe111e6711137359d4b86429153c54e2d64
Carlos Marques <catralitos11@gmail.com>
2023-06-21 17:38:37 +0100

    Added TimeManager that sends an event with time info every "hour"

---
+++ Assets/Scripts/Management/TimeManager.cs
@@ -0,0 +1,40 @@
+﻿using Events;
+using UnityEngine;
+
+namespace Management
+{
+    public class TimeManager : MonoBehaviour
+    {
+
+        public int dayInMinutes = 30;
+        public int numberOfHours = 24 + 24 + 18;
+
+        private int _currentDay = 1;
+        private int _currentHour = 6;
+        
+        private int _numberOfSeconds;
+        private float _hourLength;
+
+        [Header("Broadcasting on")]
+        public TimeChannelSO hourPassedEvent;
+        
+        public void Start()
+        {
+            _numberOfSeconds = dayInMinutes * 60;
+            _hourLength = 1.0f * _numberOfSeconds / numberOfHours;
+            InvokeRepeating(nameof(PassHour), _hourLength, _hourLength);
+        }
+
+        private void PassHour()
+        {
+            Debug.Log("An hour has passed");
+            hourPassedEvent.RaiseEvent(new TimeStep(_currentDay, _currentHour));
+            _currentHour++;
+            if (_currentHour > 24)
+            {
+                _currentDay++;
+                _currentHour = 1;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Management/TimeStep.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace Management
+{
+    [Serializable]
+    public class TimeStep
+    {
+        [Range(1,3)] public int day;
+        public int hour;
+
+        public TimeStep(int day, int hour)
+        {
+            this.day = day;
+            this.hour = hour;
+        }
+    }
+}
