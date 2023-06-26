e7e3415bcce24f0eb3ea81325b933eefba2a19a7
Carlos Marques <catralitos11@gmail.com>
2023-06-26 21:50:31 +0100

    Imported environment sprites and added collision to them.

---
+++ Assets/Scripts/Graphics/DisplayInterior.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Extensions;
+using UnityEngine;
+
+namespace Graphics
+{
+    public class DisplayInterior : MonoBehaviour
+    {
+        public GameObject outside;
+        public LayerMask playerMask;
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (playerMask.HasLayer(other.gameObject.layer))
+            {
+                outside.SetActive(!outside.activeSelf);
+            }
+        }
+    }
+}
