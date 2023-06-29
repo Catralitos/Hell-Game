d329db0f81f272fda020b765bb1ef243a7bcd07c
Carlos Marques <catralitos11@gmail.com>
2023-06-29 19:52:48 +0100

    Added music ot menus

--- Assets/Scripts/UI/Menus/LoseScreenManager.cs
+++ Assets/Scripts/UI/Menus/LoseScreenManager.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using Audio;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
@@ -8,13 +9,15 @@ namespace UI.Menus
     {
         [Header("Buttons")] public Button retryButton;
         public Button backToTitleButton;
+
+        private AudioManager _audioManager;
         
         private void Start()
         {
             retryButton.onClick.AddListener(RetryGame);
             backToTitleButton.onClick.AddListener(BackToTitle);
-            //_audioManager = GetComponent<AudioManager>();
-            //_audioManager.Play("MenuMusic");
+            _audioManager = GetComponent<AudioManager>();
+            _audioManager.Play("MenuMusic");
         }
 
         private static void RetryGame()
